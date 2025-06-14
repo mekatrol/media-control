c21fa28ff4b11ec2b9771375d1a33924d9f945e0
mekatrol <git@mekatrol.com>
2025-06-14 10:40:19 +1000

    Detect wake state

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+from typing import Tuple
 import numpy as np
 import cv2
 import pyautogui
+import math
+from digit_direction import DigitDirection
+from digit_type import DigitType
+from hand import Hand
 from hand_processor import HandProcessor, digit_names
 from hand_side import HandSide
 
 
-def draw_text_with_bg(img, text, org, font, font_scale, text_color, thickness,
-                      bg_color=(0, 0, 0), padding=4):
+def draw_text_with_bg(
+    img,
+    text: str,
+    org: Tuple[int, int],
+    font: int,
+    font_scale: float,
+    text_color: Tuple[int, int, int],
+    thickness: int,
+    bg_color: Tuple[int, int, int] = (0, 0, 0),
+    padding: int = 4
+):
     text_size, baseline = cv2.getTextSize(text, font, font_scale, thickness)
     text_width, text_height = text_size
-    x, y = org
+    x, y = map(int, org)
 
     # Rectangle coordinates
-    rect_top_left = (x - padding, y - text_height - padding)
-    rect_bottom_right = (x + text_width + padding, y + baseline + padding)
+    rect_top_left = (int(x - padding), int(y - text_height - padding))
+    rect_bottom_right = (int(x + text_width + padding),
+                         int(y + baseline + padding))
 
     # Draw background rectangle
     cv2.rectangle(img, rect_top_left, rect_bottom_right, bg_color, cv2.FILLED)
@@ -38,6 +53,34 @@ def resize_with_aspect_ratio(image, target_width, target_height):
     result[y_offset:y_offset+new_h, x_offset:x_offset+new_w] = resized
 
     return result
+
+
+vertical_hand_angle_threshold = 15
+
+
+def is_wake_state(hands: dict[HandSide, Hand]):
+    left_hand = hands[HandSide.LEFT]
+    right_hand = hands[HandSide.RIGHT]
+
+    # Both hands must be visible
+    if not left_hand.visible or not right_hand.visible:
+        return False
+
+    # Both hands must be vertical
+    if math.fabs(left_hand.angle) > vertical_hand_angle_threshold or math.fabs(right_hand.angle) > vertical_hand_angle_threshold:
+        return False
+
+    # All fingers must be up and extended (dont care about thumbs)
+    left_up_extended = all(
+        digit.direction == DigitDirection.UP and digit.extended for digit_type, digit in left_hand.digits.items() if digit_type != DigitType.THUMB)
+
+    right_up_extended = all(
+        digit.direction == DigitDirection.UP and digit.extended for digit_type, digit in right_hand.digits.items() if digit_type != DigitType.THUMB)
+
+    if not left_up_extended or not right_up_extended:
+        return False
+
+    return True
 
 
 cv2.namedWindow("Hands", cv2.WINDOW_NORMAL)
@@ -65,7 +108,7 @@ try:
 
             y_start = 25
             line_height = 25
-            
+
             # Widest possible text
             text_size, baseline = cv2.getTextSize(
                 'Gesture: Gesture.THUMBS_DOWN___', cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
@@ -102,6 +145,11 @@ try:
                             draw_text_with_bg(display_frame, f'angle: {digit.angle:.1f}', (x + 20, y),
                                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
                             y += line_height
+
+                y = y_start
+                x = screen_width / 2
+                draw_text_with_bg(display_frame, f'wake state: {is_wake_state(hands)}', (x, y),
+                                  cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
             cv2.imshow("Hands", display_frame)
 
